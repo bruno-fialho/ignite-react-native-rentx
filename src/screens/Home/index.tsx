1566cd452cbf424bf8cd016612b868c6e22fb8d1
Bruno Fialho <brugerf@gmail.com>
2022-02-21 21:53:57 -0300

    add: car card component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
@@ -11,6 +12,25 @@ import {
 } from './styles';
 
 export function Home() {
+  const carDataOne = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  };
+  const carDataTwo = {
+    brand: 'porsche',
+    name: 'Panamera S',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail: 'https://freebiescloud.com/wp-content/uploads/2021/02/PORSCHE-PANAMERA-2021-1.png'
+  };
+
   return (
     <Container>
       <Header>
@@ -24,6 +44,10 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+
+      <Car data={carDataTwo} />
     </Container>
   );
 }
