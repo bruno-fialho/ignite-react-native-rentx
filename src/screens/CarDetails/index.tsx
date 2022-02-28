fc544b36bb509c4e62e74d5955a8804c388136c8
Bruno Fialho <brugerf@gmail.com>
2022-02-28 12:13:00 -0300

    add: accessory and details components

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Accessory } from '../../components/Accessory';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -15,7 +23,8 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Accessories
 } from './styles';
 
 export function CarDetails() {
@@ -43,6 +52,15 @@ export function CarDetails() {
             <Price>R$ 500</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800cv" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Automático" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
 
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça 
