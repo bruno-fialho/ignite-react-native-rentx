38b883a8d07079d3198077f733fdfb89a5ad76a5
Bruno Fialho <brugerf@gmail.com>
2022-07-09 13:09:18 -0300

    feat: show rent cars by user

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,14 +1,39 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
+import { FlatList } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import { Car } from '../../components/Car';
 import { CarDTO } from '../../dtos/CarDTO';
 import { api } from '../../services/api';
 
 import {
-  Container
+  Container,
+  Header,
+  Title,
+  Subtitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity
 } from './styles';
 
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
+
 export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation();
+  const theme = useTheme();
+
+  function handleBack() {
+    navigation.goBack();
+  }
 
   useEffect(() => {
     async function fetchCars() {
@@ -28,7 +53,42 @@ export function MyCars() {
 
   return (
     <Container>
-      
+      <Header>
+        <BackButton 
+          color={theme.colors.shape}
+          onPress={handleBack} 
+        />
+
+        <Title>
+          Escolha uma{'\n'}
+          data de início e{'\n'}
+          fim do aluguel
+        </Title>
+
+        <Subtitle>
+          Conforto, segurança e praticidade.
+        </Subtitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>
+            Agendamentos feitos
+          </AppointmentsTitle>
+          <AppointmentsQuantity>
+            05
+          </AppointmentsQuantity>
+        </Appointments>
+
+        <FlatList
+          data={cars}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Car data={item.car} />
+          )}
+        />
+      </Content>
     </Container>
   );
 }
